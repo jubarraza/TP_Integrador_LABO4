0ae1c0b656be2d7201910717af7e1d5f9319bb41
Diego Ezequiel Benitez <104686275+Diego-Benitez@users.noreply.github.com>
2025-06-30 22:00:43 -0300

    Update GestionUsuariosServlet.java

--- src/main/java/servlet/GestionUsuariosServlet.java
+++ src/main/java/servlet/GestionUsuariosServlet.java
@@ -41,10 +41,7 @@ public class GestionUsuariosServlet extends HttpServlet {
             
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                try { conn.close(); } catch (SQLException e) { e.printStackTrace(); }
-            }
+        
         }
     }
 
